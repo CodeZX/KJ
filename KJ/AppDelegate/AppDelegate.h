45ae2b665611892433d5bcd1a1907a6d24d19f82
周鑫 <15901263220@163.com>
2018-07-12 18:22:26 +0800

    初始化

---
+++ KJ/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KJ
+//
+//  Created by 周鑫 on 2018/7/12.
+//  Copyright © 2018年 ZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KJ/Main/Controller/KJBasicSettingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KJBasicSettingTableViewController.h
+//  KJ
+//
+//  Created by 周鑫 on 2018/7/12.
+//  Copyright © 2018年 ZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KJBasicSettingTableViewController : UITableViewController
+
+@end
